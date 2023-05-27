1e73f85576924b546867e83b48c36b5a5babf872
Meriem1551 <meriem200409@gmail.com>
2023-05-27 21:55:20 +0200

    updatingProject

--- signup.js
+++ signup.js
@@ -2,18 +2,20 @@ let form=document.getElementById('email');
 let Email=document.getElementById('EMAIL');
 let containerDiv= document.getElementById("container");
 let subscribedDiv=document.getElementById("subscribed");
-let DismissButton=document.getElementById("dismissButton")
+ let DismissButton=document.getElementById("dismissButton")
+let yourEmail=document.getElementById("defaultMessage");
 let pattern=/^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 subscribedDiv.style.display="none";
 subbutton.addEventListener('click',checkValidEmail);
- function checkValidEmail(){
+function checkValidEmail(){
  if(!Email.value.match(pattern)){
     showError(Email,'valid email required');
-    subscribedDiv.style.display="none";
  }
  else{
     subscribedDiv.style.display="block";
     containerDiv.style.display="none";
+    putYourEmail(Email);
+    DismissButton.addEventListener('click',returnToFirstPage);
  }
  }
  function showError(input,message){
@@ -22,4 +24,11 @@ subbutton.addEventListener('click',checkValidEmail);
     const errorMessage=YourEmail.querySelector('small');
     errorMessage.textContent=message;
  }
-
+function putYourEmail(input){
+   yourEmail.textContent=input.value;
+}
+ function returnToFirstPage(){
+   subscribedDiv.style.display="none";
+   containerDiv.style.display="flex";
+ }
+ 
